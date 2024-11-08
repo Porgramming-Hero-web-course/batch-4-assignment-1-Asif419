9dcdaf4a93eb6f3bc61673ab0d22413f0581d3b2
asif419 <asshah419@gmail.com>
2024-11-08 18:59:35 +0200

    make the code inside a block so no minor error come forward in problem-5

--- problem-5.ts
+++ problem-5.ts
@@ -1,12 +1,15 @@
-type People = {
-    name: string;
-    age: number;
+{
+    type People = {
+        name: string;
+        age: number;
+    }
+
+    const getProperty = <T, U extends keyof T>(obj: T, key: U): any => {
+        return obj[key];
+    }
+
+
+    const person: People = { name: "Alice", age: 30 };
+    console.log(getProperty(person, "name"));
+
 }
-
-const getProperty = <T, U extends keyof T>(obj: T, key: U): any => {
-    return obj[key];
-}
-
-
-const person: People = { name: "Alice", age: 30 };
-console.log(getProperty(person, "name"));
